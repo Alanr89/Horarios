7da0910c1a8cbe929ab65650a4bede7a5d81ded5
Alanr89 <alanreyes1989@gmail.com>
2026-03-26 12:29:00 -0300

    Se agrega el .bat

---
+++ PHP/Actualizar_Sistema.bat
@@ -0,0 +1,9 @@
+@echo off
+echo ==========================================
+echo   ACTUALIZANDO SISTEMA DESDE GITHUB
+echo ==========================================
+cd C:\xampp\htdocs\HorarioLimite\Horario-main
+git pull origin main
+echo.
+echo Sistema actualizado con exito.
+pause
